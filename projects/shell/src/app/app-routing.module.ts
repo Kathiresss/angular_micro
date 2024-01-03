498f2dd0a70b89b115624f2c6abc690b9df692f6
APSDEVVPM004\ACER <kathir0813@gmail.com>
2024-01-03 13:19:46 +0530

    test

---
+++ projects/shell/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: () => loadRemoteModule({
+      remoteEntry: 'http://localhost:4202/remoteEntry.js',
+      remoteName: 'products',
+      exposedModule: './Module',
+    }).then(m => m.ProductsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
